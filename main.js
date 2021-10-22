960517d0d8368644907719e411f3a32b60c7554c
Antonioangy <dumitruangy26@gmail.com>
2021-10-22 15:57:31 +0200

    aggiunta apertura con click

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ let hamburgerBtn = document.querySelector(".fa-bars") ;
 let hamburgerClose = document.querySelector(".close") ;
 
 let hamburgerMenu = document.querySelector(".hamburger-menu") ;
+
+// apertura con click 
+hamburgerBtn.addEventListener('click',
+    function () {
+    hamburgerMenu.classList.add("active");
+  }
+)
